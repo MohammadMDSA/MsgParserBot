554ac2c7d7f1886cd36ade91d730eb594ce2eeb0
Mohammad MDSA <samimd.77@gmail.com>
2017-11-10 11:06:12 +0330

    Add mongodb handler

--- app.js
+++ app.js
@@ -8,12 +8,14 @@ server.listen(4000, () => {
 });
 
 let connector = new ChatConnector({
-	appId: '',
-	appPassword: ''
+	appId: '22c1ba29-61c8-4be1-ab68-1fdd1ab627cf',
+	appPassword: 'iFD1HuaBL6xbgj0GqCnzwDd'
 });
 
 server.post('/api/messages', connector.listen());
 
 let bot = new UniversalBot(connector, [
-	// Bot code goes here...
+	(session) => {
+		session.send('You said: ' + session.message.text);
+	}
 ]);
